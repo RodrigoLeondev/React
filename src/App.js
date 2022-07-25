924fa76c298f175d166d01ca79cadd9ad64577f0
RodrigoLeondev <rodrigoleon.dev@gmail.com>
2022-07-24 20:34:38 -0500

    add a homework, not finshd

--- src/App.js
+++ src/App.js
@@ -6,15 +6,9 @@ import List from './Components/List';
 
 function App() {
   return( <div className='App'> 
-    <List data={products}/> 
-    {/* Aqui estamos recibiendo la prop data de List y le iteramos products 
-    los productos que se mandan desde app.js se esta mandando a la lista, a través de la prop */}
-  </div>
+   </div>
 
   )
-
-
-
 }
 
 
