88a36dd43fc967c8b8b66e879ddd4da579033371
RodrigoLeondev <rodrigoleon.dev@gmail.com>
2022-07-27 22:30:45 -0500

    added form with useForm

--- src/App.js
+++ src/App.js
@@ -1,73 +1,86 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import { useState } from 'react'
-import List from './Components/List'
+import ItemsList from './Components/ItemsList'
+import products from './Mocks/products'
 
 function App () {
-  const [kodersList, setKodersList] = useState([])
-  const [koderData, setKoderData] = useState({})
+  const [allProducts, setAllProducts] = useState (products)
 
-  /*{
-    nombre:
-    generacion:
-    email
-  }*/
-  const inputHandler = event => {
-    let property = event.target.name
-    let value = event.target.value
-    console.log(`property: ${property} , value: ${value}`)
-    setKoderData({ ...koderData, [property]: value })
+  const [selectedProducts, setSelectedProducts] = useState([])
+  const addProduct = id => {
+    console.log(id)
+    const selectedProduct = products.find(product => product.id === id)
+    console.log(selectedProduct)
+    setSelectedProducts([
+      ...selectedProducts,
+      { ...selectedProduct, purchased: true }
+    ])
+  
+  }
+  const removeProduct = id => {
+    console.log(id)
+    const remainingProducts = selectedProducts.filter(
+      product => product.id !== id
+    )
+    setSelectedProducts(remainingProducts)
   }
 
-  const guardarKoder = () => {
-    setKodersList([...kodersList, koderData])
-  }
+  const categoriesList = products.reduce((accum, current) => {
+    return !accum.includes(current.category)
+      ? [...accum, current.category]
+      : accum
+  }, [])
+
+  const filtraCategoria = (event) => {
+    console.log(event.target.value)
+    setAllProducts(products)
+    console.log(allProducts )
+      const categoryProducts = products.filter(
+        product => product.category === event.target.value
+      )
+      console.log(categoryProducts)
+      setAllProducts(categoryProducts)
+    
+ }
+
+  console.log(categoriesList)
+
   return (
     <div className='App'>
       <div className='container-fluid'>
         <div className='row'>
           <div className='col-12 col-md-6'>
-            <form action='' className='bg-dark text-white p-3 border rounded'>
-              <div className='form-group'>
-                <label htmlFor=''>Nombre:</label>
-                <input
-                  type='text'
-                  className='form-control'
-                  name='nombre'
-                  onChange={inputHandler}
-                />
-              </div>
-              <div className='form-group'>
-                <label htmlFor=''>Correo</label>
-                <input
-                  type='text'
-                  className='form-control'
-                  name='email'
-                  onChange={inputHandler}
-                />
-              </div>
-              <div className='form-group'>
-                <label htmlFor=''>Generación</label>
-                <input
-                  type='text'
-                  className='form-control'
-                  name='generacion'
-                  onChange={inputHandler}
-                />
-              </div>
-              <button
-                className='btn btn-success mt-3 ms-auto'
-                onClick={guardarKoder}
-                type='button'
-              >
-                Guardar Koder
-              </button>
-            </form>
+            
+            <h1>Selección de categoría</h1>
+              <select className="form-select" aria-label="Disabled select example" onChange={filtraCategoria}>
+                <option value={categoriesList[0]}>{categoriesList[0]}</option>
+                <option value={categoriesList[1]}>{categoriesList[1]}</option>
+                <option value={categoriesList[2]}>{categoriesList[2]}</option>
+                <option value={categoriesList[3]}>{categoriesList[3]}</option>
+                <option value={categoriesList[4]}>{categoriesList[4]}</option>
+              </select>
+            
+            <h1>Catálogo de productos</h1>
+            <ItemsList
+              productsArray={allProducts}
+              addProductHandler={addProduct}
+            />
           </div>
-          <div className='col-12 col-md-6 mt-3'>
-            {!kodersList.length ? (
-              <h2>Registra un koder</h2>
+          <div className='col-12 col-md-6'>
+            <h1>Carrito de compras</h1>
+            {!selectedProducts.length ? (
+              <h2>Selecciona algún producto de la lista</h2>
             ) : (
-              <List kodersList={kodersList} />
+              <>
+                <h3>
+                  Total:
+                  {selectedProducts.reduce((acc, curr) => acc + curr.price, 0)}
+                </h3>
+                <ItemsList
+                  productsArray={selectedProducts}
+                  removeProductHandler={removeProduct}
+                />
+              </>
             )}
           </div>
         </div>
